503432ddf23fe3da1c122f21159995d6ff3ef525
lidanhong <2680143584@qq.com>
2019-11-27 11:39:49 +0800

    初始化项目

---
+++ src/components/li.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-input
+    placeholder="请输入内容"
+    v-model="input"
+    :disabled="true">
+  </el-input>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        input: ''
+      }
+    }
+  }
+
+</script>
+
+<style>
+</style>
